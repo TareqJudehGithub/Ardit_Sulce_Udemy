fd8f05ea44900da976f00a3e74af82a1818f39ac
TareqJudehGithub <tareq.joudeh@gmail.com>
2021-10-04 19:41:51 +0200

    Built a program using OOP that calculates painting amount and cost for houses.

--- Adv_Python_OOP/sec3_create_use_classes/exercises.py
+++ Adv_Python_OOP/sec3_create_use_classes/exercises.py
@@ -1,14 +1,43 @@
+
 # paint a house exercise
 class House:
+  # constructor
   def __init__(self, wall_area):
-      self.wall_area = wall_area
+    # attributes
+    self.wall_area = wall_area
+
+  # methods
+  def paint_needed(self, wall_area):
+    paint_amount = wall_area * 2.5
+    return paint_amount
+
 
 
 class Paint:
-  def __init__(self, paint_color):
-    self.paint_color = paint_color
-
-class Paint:
-  def __init__(self, bucket, color):
-      self.bucket = bucket
+  def __init__(self, buckets, color):
+      self.buckets = buckets
       self.color = color
+  
+  def total_price(self):
+    if self.color == "white".lower():
+      return self.buckets * 1.99
+    else:
+      return self.buckets * 2.19
+
+    
+
+
+# intantiate an house object
+
+villa_1 = House(10)
+villa_1_area = villa_1.wall_area
+
+# call paint_needed method
+villa_1_paint = villa_1.paint_needed(villa_1_area)
+print(villa_1_paint)
+
+# instantiate paint_1 obj
+paint_1 = Paint(color="white", buckets=4)
+
+paint_1_cost = Paint.total_price(paint_1)
+print(paint_1_cost)
--- Adv_Python_OOP/sec3_create_use_classes/sec_notes.py
+++ Adv_Python_OOP/sec3_create_use_classes/sec_notes.py
@@ -1,16 +1,24 @@
 """
-- Objects are created by classes
+- Objects are created (instantiated) by classes
 
 - self is var that holds the obj instance created by a class
+  The keyword self represents the instance of a class and binds the attributes 
+  with the given arguments.
 
 """
 class User:
+  # init method or constructor   
   def __init__(self, name, age):
+      # attributes
       self.name = name
       self.age = int(age)
       print("self: %s" % self)
 
-user_1 = User('Emma', 25)
+# initialize a instance
+
+user_1 = User('Emma', 25) # 'Emma' and 25 are passed as arguments, these \ 
+# arguments will be passed to the __init__ method to initialize the object.
+
 user_2 = User('Mike', '30')
 print(user_2.age)
     
